8fe68990fda28f07271c150c6ce068705151a210
Alexandr Shmidt <alexandr_shmidt98@vk.com>
2025-05-22 12:43:50 +0700

    реализовал модели, categorySeeder

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table = 'orders';
+    protected $fillable = [
+        'customer_name',
+        'created_at',
+        'status',
+        'customer_comment',
+    ];
+
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public $timestamps = false;
+    protected $table = 'products';
+    protected $fillable = [
+        'product_name',
+        'description',
+        'price',
+    ];
+
+
+    public function category() {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orders() {
+        return $this->hasMany(Order::class);
+    }
 }
