30143d0832b46a78e825a4c6ef4d42b2d17f9ffb
Alexandr Shmidt <alexandr_shmidt98@vk.com>
2025-05-22 15:43:31 +0700

    мелкие правки в моделях

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -14,7 +14,6 @@ class Category extends Model
         'name',
     ];
 
-
     public function products(){
         return $this->hasMany(Product::class);
     }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,6 +12,7 @@ class Product extends Model
     protected $table = 'products';
     protected $fillable = [
         'product_name',
+        'category_id',
         'description',
         'price',
     ];
