3def0b575eb5d47db05d61a18f74d4b7a2eb240b
Alexandr Shmidt <alexandr_shmidt98@vk.com>
2025-05-24 12:30:00 +0700

    реализовал static метод расчета цены

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,20 +2,45 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    use HasFactory;
     public $timestamps = false;
     protected $table = 'orders';
     protected $fillable = [
         'customer_name',
+        'product_id',
         'created_at',
         'status',
         'customer_comment',
+        'product_count',
+        'total_price',
     ];
 
 
+    const NEW = 'новый';
+    const COMPLETED = 'выполнен';
+
+
+    protected static function boot()
+    {
+            parent::boot();
+        
+            static::creating(function ($order) {
+                $order->total_price = $order->getTotalPrice();
+            });
+    }
+    
+    
+    public function getTotalPrice(): string
+    {
+        return $this->product->price * $this->product_count;
+    }
+
+    
     public function product(){
         return $this->belongsTo(Product::class);
     }
