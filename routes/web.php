245ec7e0bb689d69bf2e33c4928a5974b1128b01
Alexandr Shmidt <alexandr_shmidt98@vk.com>
2025-05-24 12:32:51 +0700

    добавил новый route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Order\OrderController;
 use App\Http\Controllers\Product\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('products', ProductController::class);
+Route::resource('orders', OrderController::class);
+
+Route::patch('/orders/complete/{order}', [OrderController::class, 'complete'],)->name('orders.complete');
