aa285dbefdafd309e79bcc117ec9746f15b532a5
Alexandr Shmidt <alexandr_shmidt98@vk.com>
2025-05-24 12:31:18 +0700

    правки в миграциях

--- database/migrations/2025_05_21_123567_create_orders_table.php
+++ database/migrations/2025_05_21_123567_create_orders_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,9 +17,11 @@ return new class extends Migration
             $table->id();
             $table->foreignId('product_id')->constrained();
             $table->string('customer_name');
-            $table->timestamp('created_at');
-            $table->string('status')->default('new');
+            $table->timestamp('created_at')->default(Carbon::now());
+            $table->string('status')->default(Order::NEW);
             $table->text('customer_comment')->nullable();
+            $table->smallInteger('product_count');
+            $table->decimal('total_price', 10, 2);
         });
     }
 
